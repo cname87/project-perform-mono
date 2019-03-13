935a6021fcc320c497cf8fc0505deea097950237
Seán A. Young <cname87@yahoo.com>
2019-03-13 21:52:15 +0000

    TEST 100%; Before client coding start

---
+++ backend/utils/utils-build/delNycFile.ts
@@ -0,0 +1,30 @@
+/**
+ * Utility to delete the mocha/istanbul nyc_output file.
+ *
+ * .nyc_output can be used to reprint the coverage report but it is easier to just look at the index.html in the coverage directory.
+ *
+ * It is generated in the workspace root.
+ *
+ * Usage:
+ * Used in package.com: npm run delNyc
+ *
+ * It will not fail if the file does not exist.
+ * npm will not see an error if the file is not deleted.
+ *
+ */
+
+import * as appRootObject from 'app-root-path';
+import fs from 'fs';
+import * as path from 'path';
+import * as shell from 'shelljs';
+
+const appRoot = appRootObject.toString();
+
+/* create path to .nyc_output file in workspace root */
+const filePath = path.join(appRoot, '..', '.nyc_output');
+
+shell.rm('-rf', filePath);
+
+if (fs.existsSync(filePath)) {
+  console.error('.nyc_output file not deleted');
+}
