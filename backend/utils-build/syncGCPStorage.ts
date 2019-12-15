ac11d0e48adcc6951b451e301999acb99df5a802
Seán A. Young <cname87@yahoo.com>
2019-12-15 10:30:49 +0000

    GCP Storage sync utility bugfix

--- backend/utils-build/syncGCPStorage.ts
+++ backend/utils-build/syncGCPStorage.ts
@@ -1,21 +1,23 @@
 /**
  * Utility to sync .env and other secret files from GCP Local Storage.
  *
- * When GCP triggers a build from github it needs to copy the .env and database certs files from a GCP Local Storage bucket which means that the file on the GCP Local Storage environment must be in sync with the ones on the local development environment.
+ * When GCP triggers a build from github it needs to copy the .env and database certs files from a GCP Local Storage bucket which means that the file on the GCP Local Storage environment must be in sync with the local development environment.
  *
- * This utility is run during backend or frontend builds:
+ * This utility is ONLY executed during development environment backend or frontend builds:
  * (i) if secret files exist (e.g. when working on the development environment) then copy them to the GCP storage replacing those on the GCP storage thus ensuring that the GCP Local Storage has the latest secret files.
- * (ii) if no secret files exist, (e.g. when working on the GCP build environment), then copy them from the GCP storage to the environment thus ensuring the GCP environment has access to the required files.
+ * (ii) if no secret files exist (e.g. deleted in error) then copy them from the GCP storage to the local development environment.
+ *
+ * Note: An environment variable set before a GCP Build ensures that the utility execution is skipped during GCP Builds.
  *
  * Usage:
  *
- * The set of files to be copied is passed in as a parameter:
- * 'env-backend' to download the backend .env files to the /.
- * 'env-frontend' to download the frontend .env files to /frontend/e2e.
- * 'db-certs' to download the datbase certs files to /backend/certs.
+ * The utility is run from an npm script:
+ * "npm run ts-node syncGCPStorage.js <parameter>".
  *
- * Called in an npm script: "npm run syncGCPStorage.js <parameter>".
- *
+ * The set of files to be synced is passed in as a parameter:
+ * 'env-backend' to download the backend .env files.
+ * 'env-frontend' to download the frontend .env files.
+ * 'db-certs' to download the datbase certs files.
  * If an incorrect, or no, parameter is passed in then an error is thrown.
  *
  */
@@ -26,12 +28,21 @@ import { Storage } from '@google-cloud/storage';
 import path from 'path';
 import fs from 'fs';
 
-/* operation passed in as the second parameter */
-/* 'env-backend' to sync backend .env files
-/* env-frontend' to sync frontend .env files and database certs file */
-const operation = process.argv[2];
+/* Note: Refer to  the switch operation below for the setting of local and remote filepaths etc */
 
-/* set GCP Local Storage credentials */
+/* set the sync operation */
+let operation = 'error';
+operation =
+  process.env.IS_GCP_BUILD === 'true'
+    ? /* if being built from GCP Build then don't sync */
+      (operation = 'no-operation')
+    : /* the operation is passed in as the second parameter */
+      /* 'env-backend' to sync backend .env files
+  /* 'env-frontend' to sync frontend .env files */
+      /* 'db-certs' to sync database certs files */
+      (operation = process.argv[2]);
+
+/* set the path to the GCP Storage credentials */
 process.env.GOOGLE_APPLICATION_CREDENTIALS = path.resolve(
   __dirname,
   '..',
@@ -42,25 +53,6 @@ process.env.GOOGLE_APPLICATION_CREDENTIALS = path.resolve(
 
 /* name of the bucket on GCP Local Storage */
 const bucketName = 'project-perform-gcp-environment-files';
-/* local directory for backend environment files relative to this file */
-const backendEnvDirectory = path.resolve(__dirname, '..', '..');
-/* local directory for frontend e2e environment files relative to this file */
-const frontendE2eEnvDirectory = path.resolve(
-  __dirname,
-  '..',
-  '..',
-  'frontend',
-  'e2e',
-);
-/* local directory for backend database certs files relative to this file */
-const databaseCertsDirectory = path.resolve(
-  __dirname,
-  '..',
-  '..',
-  'backend',
-  'certs',
-  'database',
-);
 
 export const uploadFile = async (srcFilename: string, destFilename: string) => {
   const storage = new Storage();
@@ -129,7 +121,9 @@ switch (operation) {
     const envFiles = ['.envDevelopment', '.envProduction', '.envStaging'];
     /* backend directory on GCP Storage - include '/' as POSIX-based */
     const gsDirectory = 'env-backend/';
-    operateOnFiles(envFiles, gsDirectory, backendEnvDirectory);
+    /* local directory for backend environment files relative to this file */
+    const localDirectory = path.resolve(__dirname, '..', '..');
+    operateOnFiles(envFiles, gsDirectory, localDirectory);
     break;
   }
   case 'env-frontend-e2e': {
@@ -141,7 +135,15 @@ switch (operation) {
     ];
     /* backend directory on GCP Storage - include '/' as POSIX-based */
     const gsDirectory = 'env-frontend-e2e/';
-    operateOnFiles(envFiles, gsDirectory, frontendE2eEnvDirectory);
+    /* local directory for frontend e2e .env files relative to this file */
+    const localDirectory = path.resolve(
+      __dirname,
+      '..',
+      '..',
+      'frontend',
+      'e2e',
+    );
+    operateOnFiles(envFiles, gsDirectory, localDirectory);
     break;
   }
   case 'db-certs': {
@@ -149,7 +151,20 @@ switch (operation) {
     const envFiles = ['mongoKeyAndCert.pem', 'rootCA.crt'];
     /* backend directory on GCP Storage - include '/' as POSIX-based */
     const gsDirectory = 'certs-database/';
-    operateOnFiles(envFiles, gsDirectory, databaseCertsDirectory);
+    /* local directory for backend database certs files relative to this file */
+    const localDirectory = path.resolve(
+      __dirname,
+      '..',
+      '..',
+      'backend',
+      'certs',
+      'database',
+    );
+    operateOnFiles(envFiles, gsDirectory, localDirectory);
+    break;
+  }
+  case 'no-operation': {
+    /* don't sync when running a build on GCP Build */
     break;
   }
   default: {
